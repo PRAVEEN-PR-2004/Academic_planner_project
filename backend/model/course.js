2832cdd34c454cbe34a3b88d89b84b06e9b243f3
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2025-04-26 12:22:52 +0530

    completed chapter

--- backend/model/course.js
+++ backend/model/course.js
@@ -7,6 +7,7 @@ const courseSchema = new mongoose.Schema(
     deadline: { type: Date, required: true },
     chapters: { type: Number, required: true }, // total chapters
     pendingChapters: { type: Number, required: true }, // number of chapters remaining
+    completedChapters: { type: Number, required: true },
     task: { type: String, required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     status: { type: Boolean, default: false }, // status if the course is completed
